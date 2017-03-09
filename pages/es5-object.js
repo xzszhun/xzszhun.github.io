cd2e4282b7415e16201875576c0c0053122813d9
xzszhun <xzszhun@gmail.com>
2017-03-09 18:04:52 +0800

    es5 object

---
+++ pages/es5-object.js
@@ -0,0 +1,22 @@
+// es5 新增的方法
+Object.create();
+
+Object.defineProperty()
+
+Object.getOwnPropertyDescriptor()
+
+Object.getOwnPropertyNames()
+
+Object.keys()
+
+Object.perventExtensions()
+
+Object.isExtensible()
+
+Object.seal()
+
+Object.isSealed()
+
+Object.freeze()
+
+Object.isFrozen()
--- pages/创建对象.js
+++ pages/创建对象.js
@@ -12,6 +12,8 @@ function FactoryFun (name){
 }
 var a = FactoryFun("我是工厂模式");
 
+// 工厂模式生成的无法获得对象所属类型
+// ==>
 // 构造函数模式
 function ConstructorFun(name){
   this.name = name;
@@ -19,7 +21,20 @@ function ConstructorFun(name){
 }
 var c = new ConstructorFun("我是构造函数模式");
 
+// 构造函数生成的对象，重复生成方法对象，除非把方法声明放到全局，通过变量调用方法，但这又造成了全局变量混乱（方法越来越多）
+// ==>
 // 原型模式
+function PrototypeFun(){
+}
+PrototypeFun.prototype.name = "";
+PrototypeFun.prototype.method_name = function(first_argument) {
+  // body...
+};
+var p = new PrototypeFun();
+
+// 单纯的原型模式又会导致所有生成的对象实例共用属性，在a对象上改的属性会影响到b对象上的相应属性。
+// ==>
+// 构造函数+原型 组合模式
 function PrototypeFun(){
   this.name = "";
 }
@@ -29,4 +44,27 @@ PrototypeFun.prototype.method_name = function(first_argument) {
 var p = new PrototypeFun();
 
 
+// 寄生模式|组合寄生模式
+
 /*------------------------继承---------------------------------*/
+
+function superObj(){
+  this.name = "superObj";
+  this.show = function(){
+    console.log('实例方法：'+this.name)
+  }
+}
+superObj.prototype.getName = function(){
+  console.log('原型方法：'+this.name);
+}
+function subObj(){
+  this.name = "subObj"
+}
+subObj.prototype = new superObj();
+function subObj1(){
+  this.name = "subObj1"
+}
+subObj1.prototype = new superObj();
+var so = new subObj();
+var so1 = new subObj1()
+
